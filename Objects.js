12ed871585fc9d07d269adca894287c3bb12a991
Damon McMahon <damon.mcmahon@studytafensw.edu.au>
2020-05-14 08:47:33 +1000

    Objects

---
+++ Objects.js
@@ -0,0 +1,51 @@
+// An object is a variable that holds name values i.e age, name etc
+
+// let myDog = {
+//     name: "Rex",
+//     age: 6
+// };
+
+// console.log(myDog.name);
+
+// let myDog = {
+//     name: "Rex",
+//     age: 6
+// };
+// Change name of objecy / property
+// console.log(myDog.name);
+// myDog.name = "Buffy",
+// console.log(myDog.name);
+
+// Add property to object
+
+// let myDog = {
+//     name: "Rex",
+//     age: 6
+// };
+
+// console.log(myDog.name);
+// myDog.owner = "Damon";
+// console.log(myDog);
+
+// let myDog = {
+//     name: "Rex",
+//     age: 6
+// };
+
+// console.log(myDog.name);
+
+// accessing property ways
+
+// console.log(myDog["age"]);
+
+let myDog = {
+    name: "Rex",
+    age: 6
+};
+
+console.log(myDog.name);
+
+
+
+
+
